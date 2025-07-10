e7140c60f783d206d036461b2d7e01de1b3cbfef
Juan Escalada <juanescalada175@gmail.com>
2025-07-10 16:45:34 +0900

    refactor(mui): Card component styles into MUI v5

--- src/ui/components/Card/Card.jsx
+++ src/ui/components/Card/Card.jsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import makeStyles from '@mui/styles/makeStyles';
-import styles from '../../assets/jss/material-dashboard-react/components/cardStyle';
+import { alpha, styled } from '@mui/material/styles';
+import clsx from 'clsx';
 
-const useStyles = makeStyles(styles);
+const BaseCard = styled('div')(
+  ({ theme }) => ({
+    border: '0',
+    marginBottom: '30px',
+    marginTop: '30px',
+    borderRadius: '6px',
+    color: alpha(theme.palette.common.black, 0.87),
+    background: theme.palette.common.white,
+    width: '100%',
+    boxShadow: `0 1px 4px 0 ${alpha(theme.palette.common.black, 0.14)}`,
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    minWidth: '0',
+    wordWrap: 'break-word',
+    fontSize: '.875rem',
+    '&.plain': {
+      background: 'transparent',
+      boxShadow: 'none',
+    },
+    '&.profile': {
+      marginTop: '30px',
+      textAlign: 'center',
+    },
+    '&.chart p': {
+      marginTop: '0px',
+      paddingTop: '0px',
+    },
+  })
+);
 
 export default function Card(props) {
-  const classes = useStyles();
   const { className, children, plain, profile, chart, ...rest } = props;
-  const cardClasses = classNames({
-    [classes.card]: true,
-    [classes.cardPlain]: plain,
-    [classes.cardProfile]: profile,
-    [classes.cardChart]: chart,
-    [className]: className !== undefined,
+  const cardClasses = clsx(className, {
+    plain,
+    profile,
+    chart,
   });
+
   return (
-    <div className={cardClasses} {...rest}>
+    <BaseCard className={cardClasses} {...rest}>
       {children}
-    </div>
+    </BaseCard>
   );
 }
 
