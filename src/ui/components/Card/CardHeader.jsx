b8f8b888a09a91b54d1f661d7fb4558cc37c52f8
Juan Escalada <juanescalada175@gmail.com>
2025-07-10 18:32:44 +0900

    refactor(mui): CardHeader component styles to MUI v5

--- src/ui/components/Card/CardHeader.jsx
+++ src/ui/components/Card/CardHeader.jsx
@@ -1,26 +1,87 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import makeStyles from '@mui/styles/makeStyles';
-import styles from '../../assets/jss/material-dashboard-react/components/cardHeaderStyle';
+import clsx from 'clsx';
+import { styled } from '@mui/material/styles';
 
-const useStyles = makeStyles(styles);
+const gradientMap = {
+  primary: ['#ab47bc', '#8e24aa'],
+  info: ['#26c6da', '#00acc1'],
+  success: ['#66bb6a', '#43a047'],
+  warning: ['#ffa726', '#fb8c00'],
+  danger: ['#ef5350', '#e53935'],
+  rose: ['#ec407a', '#d81b60'],
+};
+
+const BaseCardHeader = styled('div')(
+  ({ theme, ownerState }) => {
+    const { color, plain, stats, icon } = ownerState;
+    const gradient = color ? gradientMap[color] : null;
+    const isColored = color && !icon;
+
+    return {
+      padding: plain || icon || stats ? 0 : '15px',
+      margin: plain || icon || stats ? '0 15px' : '-20px 15px 0',
+      borderBottom: 'none',
+      borderRadius: isColored ? '3px' : undefined,
+      background: isColored ? `linear-gradient(60deg, ${gradient[0]}, ${gradient[1]})` : 'transparent',
+      color: isColored ? theme.palette.common.white : undefined,
+      position: 'relative',
+      zIndex: 3,
+      '&:first-of-type': {
+        borderRadius: 'calc(.25rem - 1px) calc(.25rem - 1px) 0 0',
+      },
+      ...(stats && {
+        '& svg': {
+          fontSize: '36px',
+          lineHeight: '56px',
+          width: '36px',
+          height: '36px',
+          textAlign: 'center',
+          margin: '10px 10px 4px',
+        },
+        '& .material-icons, & i': {
+          fontSize: '36px',
+          lineHeight: '56px',
+          width: '56px',
+          height: '56px',
+          textAlign: 'center',
+          marginBottom: '1px',
+        },
+        ...(icon && {
+          textAlign: 'right',
+        }),
+      }),
+      ...(icon && {
+        background: 'transparent',
+        boxShadow: 'none',
+        '& i, & .material-icons': {
+          width: '33px',
+          height: '33px',
+          textAlign: 'center',
+          lineHeight: '33px',
+        },
+        '& svg': {
+          width: '24px',
+          height: '24px',
+          textAlign: 'center',
+          lineHeight: '33px',
+          margin: '5px 4px 0px',
+        },
+      }),
+    };
+  }
+);
 
 export default function CardHeader(props) {
-  const classes = useStyles();
   const { className, children, color, plain, stats, icon, ...rest } = props;
-  const cardHeaderClasses = classNames({
-    [classes.cardHeader]: true,
-    [classes[color + 'CardHeader']]: color,
-    [classes.cardHeaderPlain]: plain,
-    [classes.cardHeaderStats]: stats,
-    [classes.cardHeaderIcon]: icon,
-    [className]: className !== undefined,
-  });
+
+  const ownerState = { color, plain, stats, icon };
+  const cardHeaderClasses = clsx(className);
+
   return (
-    <div className={cardHeaderClasses} {...rest}>
+    <BaseCardHeader className={cardHeaderClasses} ownerState={ownerState} {...rest}>
       {children}
-    </div>
+    </BaseCardHeader>
   );
 }
 
