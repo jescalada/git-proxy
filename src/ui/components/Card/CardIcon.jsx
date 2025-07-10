5d7fdeffa8d6c85d1f082fa1b3f2c1e6ed8b5dc8
Juan Escalada <juanescalada175@gmail.com>
2025-07-10 18:33:06 +0900

    refactor(mui): CardIcon styles into MUI v5

--- src/ui/components/Card/CardIcon.jsx
+++ src/ui/components/Card/CardIcon.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import makeStyles from '@mui/styles/makeStyles';
-import styles from '../../assets/jss/material-dashboard-react/components/cardIconStyle';
+import clsx from 'clsx';
+import { styled } from '@mui/material/styles';
 
-const useStyles = makeStyles(styles);
+const backgroundMap = {
+  primary: '#ab47bc',
+  info: '#26c6da',
+  success: '#66bb6a',
+  warning: '#ffa726',
+  danger: '#ef5350',
+  rose: '#ec407a',
+};
+
+const BaseCardIcon = styled('div')(({ theme, ownerState }) => {
+  const { color } = ownerState;
+  const background = backgroundMap[color] ?? theme.palette.grey[300];
+
+  return {
+    borderRadius: '3px',
+    backgroundColor: background,
+    padding: '15px',
+    marginTop: '-20px',
+    marginRight: '15px',
+    float: 'left',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white,
+  };
+});
 
 export default function CardIcon(props) {
-  const classes = useStyles();
   const { className, children, color, ...rest } = props;
-  const cardIconClasses = classNames({
-    [classes.cardIcon]: true,
-    [classes[color + 'CardHeader']]: color,
-    [className]: className !== undefined,
-  });
+
+  const ownerState = { color };
+  const cardIconClasses = clsx(className);
+
   return (
-    <div className={cardIconClasses} {...rest}>
+    <BaseCardIcon className={cardIconClasses} ownerState={ownerState} {...rest}>
       {children}
-    </div>
+    </BaseCardIcon>
   );
 }
 
